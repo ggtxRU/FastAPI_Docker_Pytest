6588c7bb8568c603e2997f156da656842e2079ff
Kirill <gularovkirill@gmail.com>
2022-06-07 19:30:28 +0300

    v0.2

---
+++ project/app/api/crud.py
@@ -0,0 +1,27 @@
+from typing import Optional, List
+
+from fastapi import HTTPException
+from app.models.pydantic import SummaryPayloadSchema
+from app.models.tortoise import TextSummary
+
+
+async def post(payload: SummaryPayloadSchema) -> int:
+    summary = TextSummary(
+        url = payload.url,
+        summary = "dummy summary",
+    )
+    await summary.save()
+    return summary.id
+
+
+async def get(id: int) -> Optional[dict]:
+    summary = await TextSummary.filter(id=id).first().values()
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+
+    return summary
+
+
+async def get_all() -> List:
+    summaries = await TextSummary.all().values()
+    return summaries
---
+++ project/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Waiting for Postgres..."
+
+
+while ! nc -z web-db 5432; do
+  sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+exec "$@"
